f5f003006e30f68398f0bafa797dada5cd3dabaa
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2023-03-29 12:23:20 +0200

    fix consignes

--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,6 @@
-import { cache } from 'react';
+export const wait = (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
 
 // 🦁 Créer un server context ici avec une propriétés "secretCode"
+
+// 🦁 Créer un server context consumer en ajoutant un petit delay via la function "wait" de 1 milliseconde
